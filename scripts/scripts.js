aa38af90d85b754d708511086e7f13ecfbf9196d
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-02-09 18:48:34 -0700

    Content Adjustments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,14 @@
 let quotes = [
-  "“One man’s ‘magic’ is another man’s engineering.” – Robert Heinlein",
-  "“Create with the heart; build with the mind.” – Criss Jami",
-  "“People can have the Model T in any color – so long as it’s black.” – Henry Ford",
-  "“The good thing about computers is that they do what you tell them to do. The bad news is that they do what you tell them to do.” – Ted Nelson",
-  "“The press is the best instrument for enlightening the mind of man, and improving him as a rational, moral and social being.” – Thomas Jefferson",
-  "“The speed of communication is wondrous to behold. It is also true that speed can multiply the distribution of information that we know to be untrue.” – Edward R. Murrow",
-  "“A good rule for rocket experimenters to follow is this: always assume that it will explode.” – Astronautics Magazine, 1937",
-  "“To achieve great things, two things are needed: a plan, and not quite enough time.” – Leonard Bernstein",
-  "“We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard.” – John F. Kennedy",
-  "“One day there will be no borders, no boundaries, no flags and no countries and the only passport will be the heart” – Carlos Santana"
+  "“One man’s ‘magic’ is another man’s engineering.”<br/><br/> – Robert Heinlein",
+  "“Create with the heart; build with the mind.”<br/><br/> – Criss Jami",
+  "“People can have the Model T in any color – so long as it’s black.”<br/><br/> – Henry Ford",
+  "“The good thing about computers is that they do what you tell them to do. The bad news is that they do what you tell them to do.”<br/><br/> – Ted Nelson",
+  "“The press is the best instrument for enlightening the mind of man, and improving him as a rational, moral and social being.”<br/><br/> – Thomas Jefferson",
+  "“The speed of communication is wondrous to behold. It is also true that speed can multiply the distribution of information that we know to be untrue.”<br/><br/> – Edward R. Murrow",
+  "“A good rule for rocket experimenters to follow is this: always assume that it will explode.”<br/><br/> – Astronautics Magazine, 1937",
+  "“To achieve great things, two things are needed: a plan, and not quite enough time.”<br/><br/> – Leonard Bernstein",
+  "“We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard.”<br/><br/> – John F. Kennedy",
+  "“One day there will be no borders, no boundaries, no flags and no countries and the only passport will be the heart”<br/><br/> – Carlos Santana"
 ];
 
 var currentQuote = 0;
